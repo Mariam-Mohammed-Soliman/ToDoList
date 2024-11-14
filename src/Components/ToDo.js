9ef672a93620f420a134ec5fc8d078e56b618202
Mariam_mohammed_soliman <mariammohammedsoliman@gmail.com>
2024-11-14 20:06:37 +0200

    edit danger to info

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -5,7 +5,7 @@ const ToDo = (props) => {
     <div className='container todoTask border mt-1 me-5'>
         <div className="row input-group justify-content-center my-3">
       <div className='col-6 mx-2 todoData border border-success' onClick={props.onToggleCompleted} style={{textDecoration:props.todo.complete?"line-through underline":""}}>{props.todo.text}</div>
-      <div className="delete mx-4 col-1 btn btn-danger" onClick={props.onDelete}>x</div>
+      <div className="delete mx-4 col-1 btn btn-info" onClick={props.onDelete}>x</div>
         </div>
     </div>
   )
