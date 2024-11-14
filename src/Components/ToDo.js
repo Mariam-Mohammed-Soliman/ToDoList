16e039f488efc86b303ab75d6fd8c039c7f61ac3
Mariam_mohammed_soliman <mariammohammedsoliman@gmail.com>
2024-11-14 20:48:32 +0200

    edit style

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -4,7 +4,7 @@ const ToDo = (props) => {
   return (
     <div className='container todoTask border mt-1 me-5'>
         <div className="row input-group justify-content-center my-3">
-      <div className='col-6 mx-2 todoData border border-success' onClick={props.onToggleCompleted} style={{textDecoration:props.todo.complete?"line-through underline":""}}>{props.todo.text}</div>
+      <div className='col-6 mx-2 todoData border-success' onClick={props.onToggleCompleted} style={{textDecoration:props.todo.complete?"line-through underline":""}}>{props.todo.text}</div>
       <div className="delete mx-4 col-1 btn btn-danger" onClick={props.onDelete}>x</div>
         </div>
     </div>
